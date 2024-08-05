e7d64229d085ada5c582a6099632525af7f2c3c5
Sohith K S <sohithsachu@gmail.com>
2024-08-06 02:46:56 +0530

    user page setup

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,33 +1,25 @@
-const { resolve } = require('path');
-const db = require('../config/connection')
-const {connect} = require('../config/connection')
+const db = require('../config/connection');
 const collection = require('../config/collection');
-
+const {connect} =require('../config/connection')
 
 module.exports = {
-    addProduct:async (product, callback) => {
-       try {  console.log('Adding product:', product);
-        const db = await connect();
-        const collection = db.collection('products'); // Access the collection
-         await collection.insertOne(product).then((data)=>{
-          // console.log(data);
-         // Insert the document
-        
-        console.log('Product added');
-        callback(data._insertedId); // Pass the result to the callback, success
-    })}
-        // Ensure the database connection is available
-    
-            catch(err) {
-                console.error('Failed to add product:', err);
-                callback(err); // Pass the error to the callback
-            }
-    },
-    getAllProducts:()=>{
-        return new Promise(async(resolve,reject)=>{
-            let products= await db.getDb().collection(collection.PRODUCT_COLLECTION).find().toArray()
-            resolve(products)
-        })
+  addProduct: async (product, callback) => {
+    try {
+      console.log('Adding product:', product);
+      const db = await connect();
+      const productCollection = db.collection('products');
+      const result = await productCollection.insertOne(product);
+      console.log('Product added with ID:', result.insertedId);
+      callback(result.insertedId); // Pass the inserted ID to the callback
+    } catch (err) {
+      console.error('Failed to add product:', err);
+      callback(null, err); // Pass null and the error to the callback
     }
-}
-
+  },
+  getAllProducts: () => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db.getDb().collection(collection.PRODUCT_COLLECTION).find().toArray();
+      resolve(products);
+    });
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,31 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var productHelpers=require('../helpers/product-helpers');
-var path = require('path')
+var productHelpers = require('../helpers/product-helpers');
+var path = require('path');
 
 /* GET users listing. */
-
-
-router.get('/', function(req, res, next) {
-  productHelpers.getAllProducts().then((products)=>{
-    //console.log(products)
-  res.render('admin/view_products',{admin:true,products})
+router.get('/', function (req, res, next) {
+  productHelpers.getAllProducts().then((products) => {
+    res.render('admin/view_products', { admin: true, products });
+  });
 });
-})
-router.get('/add-product',function(req,res){
-res.render('admin/add-product')
-})
-router.post('/add-product',function(req,res){
-productHelpers.addProduct(req.body,(id)=>{
-  let image=req.files.Image
-  image.mv('./public/product-images/'+id+'.jpg',(err) => {
-    if(!err){
-      res.redirect('/admin')
-    }else{
-      res.send("no imagesssssss")
+
+router.get('/add-product', function (req, res) {
+  res.render('admin/add-product');
+});
+
+router.post('/add-product', function (req, res) {
+  productHelpers.addProduct(req.body, (id, err) => {
+    if (err) {
+      res.send("Failed to add product");
+      return;
     }
-  })
- 
-})
-})
+    console.log('Product ID:', id);
+    if (req.files && req.files.Image) {
+      let image = req.files.Image;
+      const uploadPath = path.join(__dirname, '../public/product-images', `${id}.jpeg`);
+      image.mv(uploadPath, (err) => {
+        if (!err) {
+          res.redirect('/admin');
+        } else {
+          res.send("Failed to upload image");
+        }
+      });
+    } else {
+      res.send("No image provided");
+    }
+  });
+});
+
 module.exports = router;
